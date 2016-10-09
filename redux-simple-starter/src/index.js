cc0ca2f182f23b0f867471505a88ef0a9c79098a
Stan Chang Khin Boon <khinboon@gmail.com>
2016-10-10 02:22:05 +0800

    A Taste of JSX (1.7)

--- redux-simple-starter/src/index.js
+++ redux-simple-starter/src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new component. This component should produce some HTML
+const App = function() {
+    return <div>Hi!</div>;
+}
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-    <Provider store={createStoreWithMiddleware(reducers)}>
-        <App />
-    </Provider>
-, document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page (in the DOM)
