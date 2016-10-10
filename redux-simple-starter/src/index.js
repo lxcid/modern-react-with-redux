8e784ceaf882f65f5b0bd7b1683360a62bdb981c
Stan Chang Khin Boon <khinboon@gmail.com>
2016-10-10 08:22:54 +0800

    ES6 Import Statements + ReactDOM vs React (1.8, 1.9)

--- redux-simple-starter/src/index.js
+++ redux-simple-starter/src/index.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component. This component should produce some HTML
 const App = function() {
     return <div>Hi!</div>;
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(App);
