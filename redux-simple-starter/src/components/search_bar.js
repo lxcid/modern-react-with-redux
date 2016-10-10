04559107d594f0494aa3558e6f7296c19ce5ba6d
Stan Chang Khin Boon <khinboon@gmail.com>
2016-10-10 23:48:37 +0800

    Export Statements (1.15)

---
+++ redux-simple-starter/src/components/search_bar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SearcBar = () => {
+    return <input />
+};
+
+export default SearcBar;
--- redux-simple-starter/src/index.js
+++ redux-simple-starter/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar';
+
 import config from './config';
 
 const API_KEY = config.youtube.API_KEY;
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
