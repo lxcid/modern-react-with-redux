bfe31537736ec908b534466e1299bf1f2f865554
Stan Chang Khin Boon <khinboon@gmail.com>
2016-10-11 20:02:30 +0800

    Completion of section 1.

--- redux-simple-starter/src/components/search_bar.js
+++ redux-simple-starter/src/components/search_bar.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
         return (
-            <input onChange={(event) => console.log(event.target.value)} />
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={(event) => this.setState({ term: event.target.value })} />
+            </div>
         );
     }
 }
